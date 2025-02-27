e8c4d9466695dae46aafb26d74720b5f733f7d07
hassan1625 <hassanlatif1620@gmail.com>
2025-02-27 15:22:04 +0500

    mobile menu working

--- src/header.jsx
+++ src/header.jsx
@@ -1,13 +1,19 @@
- 
+ import React from "react"
+ import { useState } from "react"
 
 export default function Header() {
+    const [active , setActive] = useState(false)
+    const openNav = () =>{
+          setActive(!active)
+          
+    }
     return (
         <header >
             <div className="header-wrapper background-white flex align-center space-between">
                 <div className="dev-title color-black">
                     Muhammad Hassan Latif
                 </div>
-                <div className="header-nav">
+                <div className={active ? "header-nav open" : "header-nav"}>
                     <nav className="navigation">
                         <ul className="nav-list color-black gap-1 flex align-center">
                             <li><a href="#AboutOverview">About</a></li>
@@ -16,8 +22,8 @@ export default function Header() {
                         </ul>
                     </nav>
                 </div>
-                <div className="mobile-nav">
-                    <img src="/assets/menu-hamburger.svg" alt="" />
+                <div onClick={openNav} className="mobile-nav">
+                    <img  src="/assets/menu-hamburger.svg" alt="" />
                 </div>
             </div>
         </header>
